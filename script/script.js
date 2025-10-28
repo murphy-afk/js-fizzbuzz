9dd8fb16e80875513d134475a6a5af69804f09e7
murphy-afk <bardarenoemi@gmail.com>
2025-10-28 15:20:50 +0100

    feat: refactoring

--- script/script.js
+++ script/script.js
@@ -7,21 +7,33 @@
 // se è divisibile per 5 sostituire il numero con "Buzz"
 // se il numero non è divisibile per nessuno dei due stampare il numero
 
+// for (let i = 1; i <= 100; i++) {
+//   let res = i;
+//   if ((i % 3) === 0) {
+//     res = "Fizz";
+
+//     if ((i % 5) === 0) {
+//       res = "FizzBuzz";
+//     }
+//   }
+//   else {
+//     if ((i % 5) === 0) {
+//       res = "Buzz";
+//     }
+//   }
+//   console.log(res);
+// }
+
 for (let i = 1; i <= 100; i++) {
-
-  let res = i;
-
+  let res = "";
   if ((i % 3) === 0) {
     res = "Fizz";
-
-    if ((i % 5) === 0) {
-      res = "FizzBuzz";
-    }
   }
-  else {
-    if ((i % 5) === 0) {
-      res = "Buzz";
-    }
+  if ((i % 5) === 0) {
+    res = res + "Buzz";
+  }
+  if (res === "") {
+    res = i;
   }
   console.log(res);
 }
