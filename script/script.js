6d8a814282f2eb76d11d49842216bfa54b2fe8cd
murphy-afk <bardarenoemi@gmail.com>
2025-10-28 14:55:59 +0100

    feat: human code

---
+++ script/script.js
@@ -0,0 +1,8 @@
+// creare un ciclo for per scorrere i numeri da 1 a 100
+// per ogni numero controllare se è divisibile per 3 
+// se è divisibile per 3 sostituire il numero con "Fizz"
+// controllare se è anche divisibile per 5
+// se lo è sostituire "Fizz" con "Fizzbuzz"
+// se non è divisibile per 3 controllare se è divisibile per 5
+// se è divisibile per 5 sostituire il numero con "Buzz"
+// se il numero non è divisibile per nessuno dei due stampare il numero
